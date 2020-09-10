6648571a38961a823c6fa92be4e3c6b63741bfbe
crazyheqi <916615912@qq.com>
2020-09-10 16:54:07 +0800

    react init

---
+++ src/components/test.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Test = () => {
+    return (
+        <div class='test-index'>
+            <h1>标题</h1>
+            <main>内容</main>
+        </div>
+    );
+};
+
+export default Test;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,87 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./App";
+// import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>,
+//     document.getElementById("root")
+// );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
+
+// const time = new Date().toLocaleTimeString();
+
+// const str = "当前时间是:";
+
+// const element = (
+//     <div>
+//         <h1>react-demo</h1>
+//         <h2>{str + time}</h2>
+//     </div>
+// );
+
+// ReactDOM.render(element, document.querySelector("#root"));
+
+class TabComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isActive: true,
+        };
+    }
+    eventClick(val) {
+        if (val === 1) this.setState({ isActive: true });
+        else this.setState({ isActive: false });
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.eventClick.bind(this, 1)}>内容1</button>
+                <button onClick={this.eventClick.bind(this, 2)}>内容2</button>
+                {/* 下方内容 */}
+                <div
+                    style={{ display: this.state.isActive ? "block" : "none" }}>
+                    展示内容1的内容1
+                </div>
+                <div
+                    style={{
+                        display: this.state.isActive ? "none" : "block",
+                    }}>
+                    展示内容2的内容2
+                </div>
+            </div>
+        );
+    }
+}
+
+class ClockComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: new Date().toLocaleTimeString(),
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>当前时间：{this.state.time}</h1>
+            </div>
+        );
+    }
+
+    //完成渲染完成
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                time: new Date().toLocaleTimeString(),
+            });
+        }, 1000);
+    }
+}
+
+ReactDOM.render(<TabComponent />, document.querySelector("#root"));
